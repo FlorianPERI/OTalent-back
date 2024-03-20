4cbb907bd0c9afe29aa955fa275ebda8d66c2ae6
WilliamCommandeur <william.commandeur@gmail.com>
2024-03-20 16:48:36 +0100

    add(resolvers): complete member resolvers

--- app/graphql/resolvers/member.js
+++ app/graphql/resolvers/member.js
@@ -5,8 +5,18 @@ const debug = Debug('app:resolvers:member');
 const member = {
   trainings({ id: memberId }, _, { dataSources }) {
     debug(`find all trainings of member [${memberId}]`);
-    return dataSources.otalentDB.trainings.findByMemberId(memberId);
+    return dataSources.otalentDB.training.findByMemberId(memberId);
   },
+
+  categories({ id: memberId }, _, { dataSources }) {
+    debug(`find all categories of member[${memberId}]`);
+    return dataSources.otalentDB.category.findByMemberId(memberId);
+  },
+
+  reviews({ id: memberId }, _, { dataSources }) {
+    debug(`find all review of member[${memberId}]`);
+    return dataSources.otalentDB.review.findByMemberId(memberId);
+  }
 };
 
 export default member;
