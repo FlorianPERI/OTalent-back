5e91843ecbb5654fabed7743bf7ed04c4efac1b3
WilliamCommandeur <william.commandeur@gmail.com>
2024-03-20 17:02:24 +0100

    add(resolvers): complete organization resolvers

--- app/graphql/resolvers/organization.js
+++ app/graphql/resolvers/organization.js
@@ -1,2 +1,10 @@
 import Debug from 'debug';
 const debug = Debug('app:resolvers:organization');
+
+const organization = {
+    trainings({ id: organizationId }, _, { dataSources }) {
+        return dataSources.otalentDB.training.findByOrganizationId(organizationId);
+    }
+}
+
+export default organization;
