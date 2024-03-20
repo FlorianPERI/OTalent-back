32840add090dedc1c6ac242924058288bba54e84
WilliamCommandeur <william.commandeur@gmail.com>
2024-03-20 17:08:43 +0100

    add(datamappers): findByOrganizationId

--- app/graphql/dataSources/otalentDB/datamappers/Training.js
+++ app/graphql/dataSources/otalentDB/datamappers/Training.js
@@ -1,5 +1,6 @@
 import Debug from 'debug';
 import CoreDatamapper from './CoreDatamapper.js';
+import { da } from '@faker-js/faker';
 
 const debug = Debug('app:datasource:otalentDB:training');
 
@@ -15,6 +16,16 @@ class Training extends CoreDatamapper {
     debug(`finding all trainings of member[${id}]`);
     const query = {
       text: 'SELECT t.* FROM training t JOIN member_likes_training mlt ON mlt.training_id = t.id WHERE mlt.member_id = $1;',
+      values: [id],
+    };
+    const results = await this.client.query(query);
+    return results.rows;
+  }
+
+  async findByOrganizationId(id) {
+    debug(`finding all trainings of organization[${id}]`);
+    const query = {
+      text: 'SELECT * FROM training WHERE organization_id = $1;',
       values: [id],
     };
     const results = await this.client.query(query);
