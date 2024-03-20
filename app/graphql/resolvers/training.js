bdd1cd752bcd7e3c55c703ee363c1b869dfce96a
WilliamCommandeur <william.commandeur@gmail.com>
2024-03-20 16:40:51 +0100

    add(resolvers): complete resolvers for training

--- app/graphql/resolvers/training.js
+++ app/graphql/resolvers/training.js
@@ -1,2 +1,22 @@
 import Debug from 'debug';
 const debug = Debug('app:resolvers:training');
+
+const training = {
+    category({ category_id: categoryId }, _, { dataSources }) {
+        return dataSources.otalentDB.category.findByPk(categoryId);
+    },
+
+    organization({ organization_id: organizationId }, _, { dataSources }) {
+        return dataSources.otalentDB.organization.findByPk(organizationId);
+    },
+
+    members({ id }, _, { dataSources }) {
+        return dataSources.otalentDB.member.findByTrainingId(id);
+    },
+
+    reviews({ id }, _, { dataSources }) {
+        return dataSources.otalentDB.review.findByTrainingId(id);
+    },
+}
+
+export default training;
