ab845dc72594a7b799554b3b5e299ce051cab7b5
WilliamCommandeur <william.commandeur@gmail.com>
2024-03-20 16:43:24 +0100

    add(datamappers): findByTrainingId

--- app/graphql/dataSources/otalentDB/datamappers/Member.js
+++ app/graphql/dataSources/otalentDB/datamappers/Member.js
@@ -10,6 +10,16 @@ class Member extends CoreDatamapper {
     super(options);
     debug('member datamapper created');
   }
+
+  async findByTrainingId(id) {
+    debug(`find all members interested by training[${id}]`);
+    const query = {
+      text: 'SELECT m.* FROM member m JOIN member_likes_training mlt ON mlt.member_id = m.id WHERE mlt.training_id = $1;',
+      values: [id],
+    };
+    const results = await this.client.query(query);
+    return results.rows;
+  }
 }
 
 export default Member;
