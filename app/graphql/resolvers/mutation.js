3a093725760a89fa912a7a1856923c46959265cd
FlorianPERI <florian.peri@oclock.school>
2024-03-21 11:40:51 +0100

    add(resolvers): mutations

--- app/graphql/resolvers/mutation.js
+++ app/graphql/resolvers/mutation.js
@@ -2,71 +2,71 @@ import Debug from 'debug';
 
 const debug = Debug('app:resolvers:mutation');
 
-const query = {
+const mutation = {
   addMember(_, args, { dataSources }) {
-    debug(`find member [${id}]`);
-    /* return dataSources.otalentDB.member.insert(args); */
+    debug(`add new member [${args.firstname}]`);
+    return dataSources.otalentDB.member.insert(args);
   },
   modifyMember(_, { id, input }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.member.update(id, input); */
+    debug(`modify member [${id}]`);
+    return dataSources.otalentDB.member.update(id, input);
   },
   deleteMember(_, { id }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.member.delete(id); */
+    debug(`delete member [${id}]`);
+    return dataSources.otalentDB.member.delete(id);
   },
 
   addOrganization(_, args, { dataSources }) {
-    debug(`find member [${id}]`);
-    /* return dataSources.otalentDB.organization.insert(args); */
+    debug(`add new organization [${args.name}]`);
+    return dataSources.otalentDB.organization.insert(args);
   },
   modifyOrganization(_, { id, input }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.organization.update(id, input); */
+    debug(`modify organization [${id}]`);
+    return dataSources.otalentDB.organization.update(id, input);
   },
   deleteOrganization(_, { id }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.organization.delete(id); */
+    debug(`delete organization [${id}]`);
+    return dataSources.otalentDB.organization.delete(id);
   },
 
   addTraining(_, args, { dataSources }) {
-    debug(`find member [${id}]`);
-    /* return dataSources.otalentDB.training.insert(args); */
+    debug(`add new training [${args.name}]`);
+    return dataSources.otalentDB.training.insert(args);
   },
   modifyTraining(_, { id, input }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.training.update(id, input); */
+    debug(`modify training [${id}]`);
+    return dataSources.otalentDB.training.update(id, input);
   },
   deleteTraining(_, { id }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.training.delete(id); */
+    debug(`delete training [${id}]`);
+    return dataSources.otalentDB.training.delete(id);
   },
 
   addReview(_, args, { dataSources }) {
-    debug(`find member [${id}]`);
-    /* return dataSources.otalentDB.review.insert(args); */
+    debug(`add new review [${args.title}]`);
+    return dataSources.otalentDB.review.insert(args);
   },
   modifyReview(_, { id, input }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.review.update(id, input); */
+    debug(`modify review [${id}]`);
+    return dataSources.otalentDB.review.update(id, input);
   },
   deleteReview(_, { id }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.review.delete(id); */
+    debug(`delete review [${id}]`);
+    return dataSources.otalentDB.review.delete(id);
   },
 
   addCategory(_, args, { dataSources }) {
-    debug(`find member [${id}]`);
-    /* return dataSources.otalentDB.category.insert(args); */
+    debug(`add new category [${args.name}]`);
+    return dataSources.otalentDB.category.insert(args);
   },
   modifyCategory(_, { id, input }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.category.update(id, input); */
+    debug(`modify category [${id}]`);
+    return dataSources.otalentDB.category.update(id, input);
   },
   deleteCategory(_, { id }, { dataSources }) {
-    debug('find all members');
-    /* return dataSources.otalentDB.category.delete(id); */
+    debug(`delete category [${id}]`);
+    return dataSources.otalentDB.category.delete(id);
   },
 };
 
-export default query;
+export default mutation;
