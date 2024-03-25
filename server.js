0b8b5fa903145f21104b59e01443a65e7a419742
FlorianPERI <florian.peri@oclock.school>
2024-03-25 09:22:58 +0100

    add(server): create logger file and jest schema test

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import pino from 'pino';
+import pretty from 'pino-pretty';
+import SonicBoom from 'sonic-boom';
 import Fastify from 'fastify';
 import { ApolloServer } from '@apollo/server';
 import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache';
@@ -5,6 +8,7 @@ import fastifyApollo, { fastifyApolloDrainPlugin } from '@as-integrations/fastif
 import cors from '@fastify/cors';
 import Debug from 'debug';
 import { resolvers as scalarResolvers, typeDefs as scalarTypeDefs } from 'graphql-scalars';
+import dayjs from 'dayjs';
 import typeDefs from './app/graphql/schemas/index.js';
 import resolvers from './app/graphql/resolvers/index.js';
 import OtalentDB from './app/graphql/dataSources/otalentDB/datamappers/index.js';
@@ -12,9 +16,19 @@ import OtalentDB from './app/graphql/dataSources/otalentDB/datamappers/index.js'
 /**
  * Setting up the server
  */
-const PORT = process.env.SERVER_PORT ?? 3000;
+const PORT = process.env.SERVER_PORT ?? 4000;
 const debug = Debug('app:server');
 const fastify = Fastify();
+
+/**
+ * Setting up the log files
+ */
+const logger = pino(pretty({
+  colorize: false,
+  levelFirst: true,
+  translateTime: 'SYS:standard',
+  destination: new SonicBoom({ dest: `./logs/${dayjs(new Date()).format('YYYYMMDD')}.log`, mkdir: true }),
+}));
 
 /**
  * Creating an instance of ApolloServer
@@ -35,6 +49,11 @@ const apollo = new ApolloServer({
     ttl: 300, // 5 minutes
   }),
   plugins: [fastifyApolloDrainPlugin(fastify)],
+  formatError: (formattedError, error) => {
+    debug(formattedError);
+    logger.error(formattedError);
+    return formattedError;
+  },
 });
 
 /**
@@ -70,6 +89,8 @@ await fastify.register(fastifyApollo(apollo), { context: contextFunction });
 fastify.listen({ port: PORT }, (err) => {
   if (err) {
     debug(err);
+    logger.error(err);
+    process.exit(1);
   }
   debug(`🚀 Server ready at http://localhost:${PORT}/graphql`);
 });
